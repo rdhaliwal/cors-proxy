aed8fbcc4338e21659a930a13ef447687b689b8f
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2019-05-27 21:04:51 +1000

    Create another basic function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,13 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const hello = functions.https.onRequest((request, response) => {
+ response.send("hello there.");
+});
+
+export const proxy = functions.https.onRequest((request, response) => {
+  console.log('request.params', JSON.stringify(request.params));
+
+  response.send("hello there.");
 });
 
 
