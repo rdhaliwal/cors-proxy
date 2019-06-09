0c29c8dbad74ccad3a7fadeb4753ca59b776d09b
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2019-06-09 13:12:51 +1000

    feat: Fetch and return a hardcoded image

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,31 @@
 import * as functions from 'firebase-functions';
+const fetch = require('node-fetch');
 
 export const hello = functions.https.onRequest((request, response) => {
- response.send("hello there.");
+  response.set('Access-Control-Allow-Origin', '*');
+  response.set('Content-Type', 'text/json');
+  response.send({
+    "hello":  "world",
+  });
 });
 
-export const proxy = functions.https.onRequest((request, response) => {
-  response.set('Access-Control-Allow-Origin', '*');
+export const proxy = functions.https.onRequest(async (req, res) => {
+  let contentType: string|null = 'image/jpeg';
+  const imageUrl: string ='https://media.giphy.com/media/Lam1vPSI8R3DG/giphy.gif';
 
-  if (request.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
-    response.set('Access-Control-Allow-Methods', 'GET');
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.set('Access-Control-Max-Age', '3600');
-    response.status(204).send('');
-  } else {
-    response.send(`You want to proxy a request to ${request.params[0]}`);
-  }
+  return await fetch(imageUrl, {})
+    .then((r: Response) => {
+      contentType = r.headers.get('content-type');
+
+      return r.arrayBuffer();
+    })
+    .then((buffer: ArrayBuffer) => {
+      res.set('Access-Control-Allow-Origin', '*');
+      if (contentType != null) {
+        res.writeHead(200, {'Content-Type': contentType });
+      }
+      res.end(Buffer.from(buffer), 'binary');
+    });
 });
 
 
